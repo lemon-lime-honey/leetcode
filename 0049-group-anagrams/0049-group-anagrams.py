fadd4ce55f7f73099a6d8129d9f520897d128c46
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-08-31 21:20:18 +0900

    Time: 111 ms (47.24%), Space: 22.4 MB (8.22%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,22 +1,16 @@
-from collections import Counter
+from collections import defaultdict
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = list()
-        words = dict()
+        result = defaultdict(list)
 
-        for word in strs:
-            words[word] = sorted(Counter(word).most_common())
+        for s in strs:
+            count = [0 for i in range(26)]
 
-        for word in strs:
-            if not result:
-                result.append([word])
-            else:
-                for lis in result:
-                    if words[word] == words[lis[-1]]:
-                        lis.append(word)
-                        break
-                else:
-                    result.append([word])
-        return result
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            result[tuple(count)].append(s)
+
+        return result.values()
