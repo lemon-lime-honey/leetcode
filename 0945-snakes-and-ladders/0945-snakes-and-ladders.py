2124ab4088097ae950f6fce7c70e79bac85e8855
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-09-14 20:22:24 +0900

    Time: 139 ms (28.15%), Space: 16.3 MB (82.61%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -3,27 +3,33 @@ from collections import deque
 
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n = len(board)
-        newboard = [0]
-        for i in range(n):
-            if i % 2:
-                newboard.extend(board[n - 1 - i][::-1])
-            else:
-                newboard.extend(board[n - 1 - i])
-        visited = [int(1e9) for i in range(n ** 2 + 1)]
-        que = deque([1])
-        visited[1] = 0
+        def conversion(number):
+            r = (number - 1) // n
+            c = (number - 1) % n
+            if r % 2: c = n - 1 - c
 
+            return (r, c)
+
+
+        board = board[::-1]
+        n = len(board)
+        que = deque([1])
+        visited = [int(1e9) for i in range(n ** 2 + 1)]
+        visited[1] = 0
+        
         while que:
-            point = que.popleft()
+            now = que.popleft()
 
             for i in range(1, 7):
-                next_point = point + i
-                if next_point < n ** 2 + 1:
-                    if newboard[next_point] != -1:
-                        next_point = newboard[next_point]
-                    if visited[next_point] > visited[point] + 1:
-                        visited[next_point] = visited[point] + 1
-                        que.append(next_point)
+                point = now + i
+                nr, nc = conversion(point)
 
-        return -1 if visited[n ** 2] == int(1e9) else visited[n ** 2]
+                if 0 <= nr < n and 0 <= nc < n:
+                    if board[nr][nc] != -1:
+                        point = board[nr][nc]
+                    if point == n ** 2: return visited[now] + 1
+                    if visited[point] > visited[now] + 1:
+                        visited[point] = visited[now] + 1
+                        que.append(point)
+
+        return -1
