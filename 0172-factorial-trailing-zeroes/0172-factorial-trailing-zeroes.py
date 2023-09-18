504c88ea9d18e14430139b423b89f7111628b621
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-09-18 19:30:08 +0900

    Time: 41 ms (65.61%), Space: 16.1 MB (88.04%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,18 +1,10 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        if n < 5: return 0
+        result = 0
 
-        two, five = 0, 0
+        while n:
+            if n < 5: break
+            result += n // 5
+            n //= 5
 
-        for i in range(2, n + 1):
-            if not i % 2 or not i % 5:
-                chk = i
-                while not (chk % 2 and chk % 5):
-                    if not chk % 2:
-                        chk //= 2
-                        two += 1
-                    if not chk % 5:
-                        chk //= 5
-                        five += 1
-
-        return min(two, five)
+        return result
