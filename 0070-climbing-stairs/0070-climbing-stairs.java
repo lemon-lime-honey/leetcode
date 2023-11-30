eb9786ec20489c7851dc67667a00b8e247657452
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-11-30 11:00:13 +0900

    Time: 0 ms (100.00%), Space: 39.5 MB (18.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        List<Integer> dp = new ArrayList<>();
-        dp.add(1);
-        dp.add(2);
-
-        for (int i = 2; i < n; i++) {
-            dp.add(dp.get(dp.size() - 1) + dp.get(dp.size() - 2));
+        if (n == 1) {
+            return 1;
         }
 
-        return dp.get(n - 1);
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n - 1];
     }
 }
