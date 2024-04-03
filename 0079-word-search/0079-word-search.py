c7f99500fa6da1e2e63fb47a83df4c385d06bd88
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-04-03 19:24:06 +0900

    Time: 2988 ms (87.09%), Space: 16.6 MB (71.59%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,33 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def search(r, c, route, idx):
-            nonlocal result
-            if len(route) == len(word):
-                result = True
-                return
-            for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                nr, nc = r + dr, c + dc
-                if (0 <= nr < n) and (0 <= nc < m):
-                    if not visited[nr][nc] and board[nr][nc] == word[idx + 1]:
-                        visited[nr][nc] = True
-                        search(nr, nc, route + word[idx + 1], idx + 1)
-                        if result: return
-                        visited[nr][nc] = False
+        def search(r, c, idx):
+            if idx == len(word):
+                return True
+
+            if (r < 0 or r >= n or c < 0 or c >= m or
+                visited[r][c] or board[r][c] != word[idx]):
+                return False
+
+            visited[r][c] = True
+
+            if (search(r + 1, c, idx + 1) or
+                search(r - 1, c, idx + 1) or
+                search(r, c + 1, idx + 1) or
+                search(r, c - 1, idx + 1)):
+                return True
+
+            visited[r][c] = False
+            return False
+
 
         n, m = len(board), len(board[0])
         visited = [[False for i in range(m)] for j in range(n)]
         result = False
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
+        for i in range(n):
+            for j in range(m):
                 if board[i][j] == word[0]:
-                    visited[i][j] = True
-                    search(i, j, word[0], 0)
-                    visited[i][j] = False
+                    result = search(i, j, 0)
+                    if result: return True
 
-        return result
+        return False
