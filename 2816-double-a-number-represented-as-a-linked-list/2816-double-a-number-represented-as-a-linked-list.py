cbf2c4e6f468db9010ef7c2948b1f0878b26352f
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-05-07 12:17:56 +0900

    Time: 229 ms (73.55%), Space: 20.6 MB (10.48%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
@@ -1,27 +1,9 @@
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node = head
-        target = 0
+        def solve(node: Optional[ListNode]) -> int:
+            value = 2 * node.val + (solve(node.next) if node.next else 0)
+            node.val = value % 10
+            return value // 10
 
-        while node:
-            target = target * 10 + node.val
-            node = node.next
 
-        target *= 2
-        node = head
-        number = list()
-
-        if target == 0: number.append(0)
-
-        while target:
-            number.append(target % 10)
-            target //= 10
-
-        while node:
-            node.val = number.pop()
-            if number:
-                if not node.next:
-                    node.next = ListNode(-1)
-            node = node.next
-
-        return head
+        return ListNode(1, head) if solve(head) else head
