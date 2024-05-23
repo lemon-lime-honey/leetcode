d9b232014cd7040a1023c3dc0e8c3e0f9eb8cffb
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-05-23 17:14:54 +0900

    Time: 1930 ms (72.85%), Space: 16.6 MB (53.29%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -1,25 +1,21 @@
 class Solution:
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
         def backtrack(idx, sub):
-            if sub:
-                nonlocal result
-                result += 1
-
             if idx == len(nums):
+                if sub:
+                    nonlocal result
+                    result += 1
                 return
 
-            for i in range(idx, len(nums)):
-                flag = False
-                for n in sub:
-                    if abs(nums[i] - n) == k:
-                        flag = True
-                        break
-                if flag: continue
-                sub.append(nums[i])
-                backtrack(i + 1, sub)
+            if (nums[idx] - k) not in sub:
+                sub.append(nums[idx])
+                backtrack(idx + 1, sub)
                 sub.pop()
 
+            backtrack(idx + 1, sub)
 
+
+        nums.sort()
         result = 0
         backtrack(0, list())
         return result
