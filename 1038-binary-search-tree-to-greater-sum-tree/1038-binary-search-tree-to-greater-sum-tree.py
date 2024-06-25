fa6348d269e5b6b7a92b7739a15819c05c750ce3
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-06-25 21:58:41 +0900

    Time: 33 ms (78.02%), Space: 16.4 MB (74.62%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,9 @@
+class Solution:
+    val = 0
+
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        if root.right: self.bstToGst(root.right)
+        root.val += self.val
+        self.val = root.val
+        if root.left: self.bstToGst(root.left)
+        return root
