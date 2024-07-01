dca332af735732ebb3dfd84880892ec00091da8e
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-07-01 15:20:02 +0900

    Time: 0 ms (100%), Space: 41.9 MB (73.84%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int cnt = 0;
+
+        for (int num: arr) {
+            if (num % 2 == 1) cnt++;
+            else cnt = 0;
+            if (cnt == 3) return true;
+        }
+
+        return false;
+    }
+}
