6c466f211c7642ec0d30274bdeb22fde6af40063
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-08-19 12:18:15 +0900

    Time: 5 ms (64.07%), Space: 40.8 MB (29.96%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSteps(int n) {
+        int[] result = new int[n + 1];
+
+        for (int i = 2; i < n + 1; i++) {
+            result[i] = i;
+            for (int j = i / 2; j > 1; j--) {
+                if (i % j == 0) {
+                    result[i] = result[j] + i / j;
+                    break;
+                }
+            }
+        }
+
+        return result[n];
+    }
+}
