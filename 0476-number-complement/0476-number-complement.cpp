7537fc151a3ebcc07d4d5a8585c5bb1f78cf6ed9
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-08-22 11:22:29 +0900

    Time: 0 ms (100%), Space: 7.6 MB (33.08%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        unsigned int mask = ~0;
+
+        while (num & mask) {
+            mask <<= 1;
+        }
+
+        return ~num ^ mask;
+    }
+};
