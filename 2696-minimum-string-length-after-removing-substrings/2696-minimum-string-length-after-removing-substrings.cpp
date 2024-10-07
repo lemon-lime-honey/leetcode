2cdd36bc6dd4758fec0e46f4ed897a458d42770b
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-10-07 21:33:51 +0900

    Time: 0 ms (100%), Space: 11.5 MB (72.37%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++) {
+           if (st.empty()) {
+            st.push(s[i]);
+            continue;
+           }
+
+           if (s[i] == 'B' && st.top() == 'A') {
+            st.pop();
+           } else if (s[i] == 'D' && st.top() == 'C') {
+            st.pop();
+           } else {
+            st.push(s[i]);
+           }
+        }
+
+        return st.size();
+    }
+};
