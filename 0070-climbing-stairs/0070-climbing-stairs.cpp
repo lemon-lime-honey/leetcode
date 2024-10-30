b1796ab85370a238ef3f69d0548ac99f1fa6afb6
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-10-30 23:23:01 +0900

    Time: 0 ms (100%), Space: 7.4 MB (88.44%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+
+        int now = 1, before = 1;
+
+        for (int i = 1; i < n; i++) {
+            int temp = now;
+            now = temp + before;
+            before = temp;
+        }
+
+        return now;
+    }
+};
