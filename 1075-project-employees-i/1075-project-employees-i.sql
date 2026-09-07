7b4381c6c7dae84ecdb54219d1331a1b7452722c
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2026-09-07 10:40:12 +0900

    Time: 512 ms (83.89%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,5 @@
+SELECT project_id, ROUND((SUM(e.experience_years) / COUNT(p.employee_id)), 2) AS average_years
+FROM Project p
+LEFT OUTER JOIN Employee e
+  ON p.employee_id = e.employee_id
+GROUP BY project_id;
